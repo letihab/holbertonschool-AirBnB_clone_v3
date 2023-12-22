289366471b8d458645cf8fde5f7d95619771d6c6
nathalie <nathalie.metreau@hotmail.fr>
2023-12-22 17:11:36 +0100

    correct things

--- tests/test_console.py
+++ tests/test_console.py
@@ -19,7 +19,6 @@ class TestConsoleDocs(unittest.TestCase):
         self.assertEqual(result.total_errors, 0,
                          "Found code style errors (and warnings).")
 
-
     def test_pep8_conformance_test_console(self):
         """Test that tests/test_console.py conforms to PEP8."""
         style = pycodestyle.StyleGuide(quiet=True)
@@ -27,14 +26,12 @@ class TestConsoleDocs(unittest.TestCase):
         self.assertEqual(result.total_errors, 0,
                          "Found code style errors")
 
-
     def test_console_module_docstring(self):
         """Test for the console.py module docstring"""
         self.assertIsNot(console.__doc__, None,
                          "console.py needs a docstring")
         self.assertTrue(len(console.__doc__) >= 1,
                         "console.py needs a docstring")
-
 
     def test_HBNBCommand_class_docstring(self):
         """Test for the HBNBCommand class docstring"""
