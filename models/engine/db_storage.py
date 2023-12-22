c410946b57d6b4d1e19d9e6c85ba40e6e5c54026
nathalie <nathalie.metreau@hotmail.fr>
2023-12-22 16:35:31 +0100

    class imports on storages

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -3,10 +3,10 @@
 Contains the class DBStorage
 """
 
-import models
 from models.amenity import Amenity
 from models.base_model import BaseModel, Base
 from models.city import City
+import models
 from models.place import Place
 from models.review import Review
 from models.state import State
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,16 +3,16 @@
 Contains the FileStorage class
 """
 
-import json
-import models
 from models.amenity import Amenity
 from models.base_model import BaseModel
 from models.city import City
+from hashlib import md5
+import json
+import models
 from models.place import Place
 from models.review import Review
 from models.state import State
 from models.user import User
-from hashlib import md5
 
 classes = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
            "Place": Place, "Review": Review, "State": State, "User": User}
